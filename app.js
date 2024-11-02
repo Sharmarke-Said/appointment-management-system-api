618e7ec9a1f9faa261637bfcc17e714edde90d4b
Sharmarke-Said <sharmakesaid17@gmail.com>
2024-11-02 10:21:23 +0300

    changes

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const categoryRouter = require("./routes/categoryRoutes");
+const userRouter = require("./routes/userRoutes");
+const appointmentRouter = require("./routes/appointmentRoutes");
+
+const app = express();
+app.use(express.json());
+
+// Enable CORS
+app.use(cors());
+
+// Use routes
+app.use("/api/categories", categoryRouter);
+app.use("/api/users", userRouter);
+app.use("/api/appointments", appointmentRouter);
+
+module.exports = app;
